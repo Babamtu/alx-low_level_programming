88f3e0ec67adb4445c13349a1bb9c62d5f3c1b4a
Babamtu <natemachoka@gmail.com>
2023-02-16 10:45:48 -0800

    printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ *main - entry point
+ *a program that uses printf to print a line
+ *Return: 0 (Success)
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
