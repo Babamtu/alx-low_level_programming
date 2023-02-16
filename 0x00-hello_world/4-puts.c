0d5e0f7a67a1ba5e55342d497eaf2d5718adb48d
Babamtu <natemachoka@gmail.com>
2023-02-16 10:14:06 -0800

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,13 @@
-# include <stdio.h>
+#include <stdio.h>
+
 /**
- *main -Entry point
- *A program that prints a line with puts
- *Return: 0
+ *main  Entry point
+ *a program that prints a line using puts
+ *Return: 0 (success)
  */
+
 int main(void)
 {
-	puts("/"Programming is like building a multilingual puzzle");
+puts("\"Programming is like building a multilingual puzzle");
 return(0);
 }
