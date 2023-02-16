b76034e0dcd5d9a7bb8446b4215d41b5d00137aa
Babamtu <natemachoka@gmail.com>
2023-02-16 12:52:34 -0800

    quote

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <unistd.h>
+
+/**
+ *main -Entry point
+ *a program that prints to the std error
+ *should have no warnings from Betty wall
+ *Return: 1 (Success)
+ */
+
+int main(void)
+{
+	fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+	return (1);
+}
