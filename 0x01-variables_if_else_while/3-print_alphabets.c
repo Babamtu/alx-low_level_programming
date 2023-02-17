fbf02840c9a89dd947b0186e673ea388f096b682
Babamtu <natemachoka@gmail.com>
2023-02-17 09:41:39 -0800

    lowercase and uppercase

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+/**
+ *main - Entry point
+ *
+ *a program to print capital and small letters using putchar
+ *Return: 0 (Success)
+ */
+
+int main(void)
+{
+	char c;
+
+	char d;
+
+	c = 'a';
+	d = 'A';
+	while
+		(c <= 'z') {
+		putchar(c);
+		c++;
+	}
+	while
+		(d <= 'z') {
+		putchar(c);
+		d++;
+	}
+	return (0);
+}
+
