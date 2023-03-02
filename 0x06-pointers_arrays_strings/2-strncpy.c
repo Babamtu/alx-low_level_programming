9a0de3804d51f7259ed638fe9ea1d20993904d0c
Babamtu <natemachoka@gmail.com>
2023-03-02 00:51:51 -0800

    strcpy

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * _strncpy - a function that copies from src to dest
+ * @src: source file
+ * @dest: destination file
+ * @n: copy limit
+ *
+ * Return: pointer to destinatiom
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i;
+
+	for (i = 0; i < n && src[i] != '\0'; i++)
+	{
+		dest[i] = src[i];
+		dest++;
+	}
+	for (; i < n; i++)
+	{
+		dest[i] = '\0';
+	}
+
+	return (dest);
+}
